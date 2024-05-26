38070952c0cb32cd509f24d962132d0859cb563f
Ingrid Sætersdal <127530832+Ingridsaeters@users.noreply.github.com>
2024-05-26 14:27:23 +0200

    Update lambda_PC.R

--- 03_modelling_niches/lambda_PC.R
+++ 03_modelling_niches/lambda_PC.R
@@ -21,15 +21,15 @@ setwd()
 
 # Read trees and data ----
 #________________
-## Assign trees
-trees <- list.files("/path/to/treefiles")
+## List treefiles
+trees <- list.files("/path/to/treefile/folder")
 
 ## Read data
-### PC scores
-PCfiles<-list.files("/path/to/PC_files")
+### List PC-scores files
+PCfiles<-list.files("/path/to/PC_file/folder")
 
-### Measurement error files (Proceedure from Drury et al. 2018 to calculate measurement errors for PC scores)
-mefiles<-list.files("/path/to/me_files")
+### List measurement error files (Proceedure from Drury et al. 2018 to calculate measurement errors for PC scores)
+mefiles<-list.files("/path/to/me_file/folder")
 
 # Calculate Pagel's lambda and probability that lambda is significantly different from 0 ----
 #____________________________________________________________________________________________
@@ -39,17 +39,17 @@ results_df<-tibble()
 ## Calculate lambda by using the first tree file in the tree folder, the first PC file in the PC folder and first me file in me folder. Then the second and so on
 for(i in seq_along(trees))  {
   # Read tree
-  tree_path<-file.path("/Users/ingri/OneDrive/Dokumenter/ciliate_niche/analyses/Analyses/dating/marine_pruned", trees[[i]])
+  tree_path<-file.path("/path/to/treefile/folder", trees[[i]])
   tree<-read.newick(tree_path)
   tree_name<-basename(trees[[i]])
   tip_labels<-tree$tip.label
   
   # Read data 
-    data_path<-file.path("/Users/ingri/OneDrive/Dokumenter/ciliate_niche/analyses/Analyses/modelling/pPCA/pc5_scores_marine", PCfiles[[i]])
+    data_path<-file.path("/path/to/PC_file/folder", PCfiles[[i]])
     PC <-read.csv(data_path, sep = "\t")
   
     # Read me file
-    me_path<-file.path("/Users/ingri/OneDrive/Dokumenter/ciliate_niche/analyses/Analyses/modelling/pPCA/me_marine", mefiles[[i]])
+    me_path<-file.path("/path/to/me_file/folder", mefiles[[i]])
     me<-read.csv(me_path, sep=",")
     rownames(me)<-me$ASV
     me_reordered<-me[match(rownames(PC1), rownames(me)), , drop=FALSE]
