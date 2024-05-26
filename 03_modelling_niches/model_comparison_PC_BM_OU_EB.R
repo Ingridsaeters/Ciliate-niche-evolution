7e9fccfb0e2e308fa554ab06fa2ebf6bd8323dbc
Ingrid Sætersdal <127530832+Ingridsaeters@users.noreply.github.com>
2024-05-26 19:49:57 +0200

    Update model_comparison_PC_BM_OU_EB.R

--- 03_modelling_niches/model_comparison_PC_BM_OU_EB.R
+++ 03_modelling_niches/model_comparison_PC_BM_OU_EB.R
@@ -24,41 +24,42 @@ setwd()
 
 # Read trees and data ----
 #_________________________
-## Read trees
-trees <- list.files("/path/to/treefile_folder")
+## List trees
+trees <- list.files("/path/to/tree/folder")
 
-## Read files with PC scores
-traits <- list.files("/path/to/PCfiles_folder")
+## List files with PC scores
+traits <- list.files("/path/to/PC/folder")
 
-# Read files with measurement errors (Proceedure from Drury et al. 2018 to calculate measurement errors for PC scores)
-sems <- list.files("/path/to/me_files")
+# Read files with measurement errors (see proceedure from Drury et al. 2018 to calculate measurement errors for PC scores)
+sems <- list.files("/path/to/me/folder")
 
 # Perform model test ----
 #________________________
 ## Make an empty tibble to store results
 results_df<-tibble()
 
-# Perform model test by using the first tree file in the tree folder, the first PC file in the PC folder and first me file in me folder. Then the second and so on
+## Make a loop though the first tree file in the tree folder, the first PC file in the PC folder and first me file in me folder. 
+## Then repeat for the second and so on.
 for (i in seq_along(trees)) {
-  tree_path <- file.path(tree_directory, trees[[i]])
+  tree_path <- file.path("/path/to/tree/folder", trees[[i]])
   tree <- read.tree(tree_path)
   tree_filename<- basename(trees[[i]])
   tip_labels<- tree$tip.label
   
   # Read me file
-  sem <- read.csv(file.path("/path/to/me_files", sems[[i]]), sep = ",")
+  sem <- read.csv(file.path("/path/to/me/folder", sems[[i]]), sep = ",")
   colnames(sem)[1]<-"ASV"
-  colnames(sem)[2]<-"pc_values"
+  colnames(sem)[2]<-"sem_values"
   
   # Order me file by order in tree
   sem_reordered <- sem[match(tip_labels, sem$ASV), ,drop = FALSE]
 
   # Make a named vector with sem
-  sem_vector <- sem_reordered$pc_values
- names(sem_vector) <- tip_labels
+  sem_vector <- sem_reordered$sem_values
+  names(sem_vector) <- tip_labels
 
  # Read PC file
-trait <- read.csv(file.path("/path/to/PCfiles_folder", traits[[i]]), sep = "\t")
+trait <- read.csv(file.path("/path/to/PC/folder", traits[[i]]), sep = "\t")
 
 # Order by order in tree
 trait_reordered <- trait[match(tip_labels, rownames(trait)), , drop = FALSE]
@@ -68,9 +69,10 @@ colnames(trait_reordered)[1]<- "pc"
 vector <- trait_reordered$pc
 names(vector) <- rownames(trait_reordered)
 
-# Perform model analysis (Here exemplified with BM. Change to OU and EB if you wish to use these model)
+# Perform model analysis (Here exemplified with BM. Change to OU and EB for analysis with these model)
 BM.fit <- fit_t_standard(tree, vector, model = "BM", error = sem_vector)
-                              
+
+# Extract output
 LH <- BM.fit$logl
 aic <- BM.fit$aic
 aicc <- BM.fit$aicc
@@ -92,4 +94,3 @@ theta = theta)
 results_df <- bind_rows(results_df,trait_results)
 write.csv(results_df, "name_of_file.csv")
 }
-
