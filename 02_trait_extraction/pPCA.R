21409899f2d7e88872ac129ee340f4fccdaad5ed
Ingrid Myklestad Sætersdal <ingrmsae@login-4.saga>
2024-05-26 14:11:26 +0200

    New files

--- 02_trait_extraction/pPCA.R
+++ 02_trait_extraction/pPCA.R
@@ -25,37 +25,102 @@ library(readr)
 ## Set working directory
 setwd("C:/Users/path/to/directory")
 
-# Read tree ----
-#_______________
-tree<-readNewick("name_of_tree_file.nwk", convert.edge.length = TRUE)
-plot(tree)
+# Assign path to tree files ----
+#_______________________________
+trees<-list.files("/path/to/treefiles")
 
 # Read data ----
 #_______________
-data<-read.table("name_of_traits_file.tsv", header = TRUE, sep = "\t")
-data<-data[-2] #Taxon is not a variable to be included in PCA analysis
-row.names(data)<-data$ASV
-data<-data[-1]
+data<-read_tsv()
+### Make sure rownames of data is ASVs
 
-# Combine tree and data ----
-#___________________________
-combine_tree_data <- phylobase::phylo4d(tree, data)
-
-# Perform phylogenetic PCA ----
-#______________________________
-pca <- adephylo::ppca(combine_tree_data, scale = FALSE, scannf = FALSE, nfposi = 2, method = "Abouheif") 
-### This takes a while....
-print(pca)
-
-# Extract PCA scores ----
-#________________________
-pca_scores<-pca$li
-pca_scores["ASV"]<-row.names(pca_scores)
-row.names(pca_scores)<-NULL
-
-# Write file ----
-#________________
-write_tsv(pca_scores, "name_of_output_file.tsv")
+## Subset into groups if needed
+data_subset <- data %>% 
+  dplyr::select("trait1", "trait2", "trait3", "etc")
 
 
+# Perform pPCA ----
+#__________________
+## Make empty tibbles to store results
+df <- tibble()
+results_df <- tibble()
 
+## Loop through all trees, perform pPCA and extract PC scores and percent variation
+for (tree_file in trees) {
+  # Read tree file
+  tree_path <- path("/Users/ingri/OneDrive/Dokumenter/ciliate_niche/analyses/Analyses/dating/marine_pruned", tree_file)
+  tree<-read.newick(tree_path)
+  tree_name<-basename(tree_file)
+  tip_labels<-tree$tip.label
+
+  # Reorder data so it is in the same order as the tree file
+  data_reordered<-data[match(tip_labels, rownames_data), , drop = FALSE]
+  data_reordered<-as.matrix(data_reordered)
+  
+  # Perform phylogenetic PCA 
+  pca <- phyl.pca(tree, data_reordered, method = "BM", mode = "cov") 
+ 
+  # Percent of variation explained by the first five PC axes
+  pc1<-pca$Eval[,1]/sum(pca$Eval)
+  pc2<-pca$Eval[,2]/sum(pca$Eval)
+  pc3 <- pca$Eval[,3]/sum(pca$Eval)
+  pc4 <- pca$Eval[,4]/sum(pca$Eval)
+  pc5 <- pca$Eval[,5]/sum(pca$Eval)
+  pc1<-as.numeric(pc1[1])
+  pc2<-as.numeric(pc2[2])
+  pc3 <- as.numeric(pc3[3])
+  pc4 <- as.numeric(pc4[4])
+  pc5 <- as.numeric(pc5[5])
+  
+  # Store the results of percent variation in a tibble
+  df <- tibble(
+    tree = tree_name, 
+    pc1 = pc1, 
+    pc2 = pc2,
+    pc3 = pc3, 
+    pc4 = pc4, 
+    pc5 = pc5
+  )
+  # Write out a file with percent variation
+  results_df <- dplyr::bind_rows(results_df, df)
+  write.csv(results_df, "marine_precent_variation.csv")
+  
+  # Get PCA scores
+  pca_scores<-data.frame(pca$S)
+  
+  # Write file with PC1 scores
+  pc1_scores<- data.frame(pca_scores$PC1)
+  colnames(pc1_scores)[1]<-tree_name
+  rownames(pc1_scores)<-rownames(pca_scores)
+  # Save PC1 scores to file
+  write.table(pc1_scores, file = paste0("pc1_scores_marine/pc3_scores_", tree_name, ".tsv"), sep = "\t", row.names = TRUE, col.names = TRUE)
+
+  # Write file with PC2 scores
+  pc2_scores<- data.frame(pca_scores$PC2)
+  colnames(pc2_scores)[1]<-tree_name
+  rownames(pc2_scores)<-rownames(pca_scores)
+  # Save PC1 scores to file
+  write.table(pc2_scores, file = paste0("pc2_scores_marine/pc3_scores_", tree_name, ".tsv"), sep = "\t", row.names = TRUE, col.names = TRUE)
+
+  # Write file with PC3 scores
+  pc3_scores<- data.frame(pca_scores$PC3)
+  colnames(pc3_scores)[1]<-tree_name
+  rownames(pc3_scores)<-rownames(pca_scores)
+  # Save PC1 scores to file
+  write.table(pc3_scores, file = paste0("pc3_scores_marine/pc3_scores_", tree_name, ".tsv"), sep = "\t", row.names = TRUE, col.names = TRUE)
+  
+  # Write file with PC4 scores
+  pc4_scores<- data.frame(pca_scores$PC4)
+  colnames(pc4_scores)[1]<-tree_name
+  rownames(pc4_scores)<-rownames(pca_scores)
+  # Save PC1 scores to file
+  write.table(pc4_scores, file = paste0("pc4_scores_marine/pc4_scores_", tree_name, ".tsv"), sep = "\t", row.names = TRUE, col.names = TRUE)
+  
+  # Write file with PC5 scores
+  pc5_scores<- data.frame(pca_scores$PC5)
+  colnames(pc5_scores)[1]<-tree_name
+  rownames(pc5_scores)<-rownames(pca_scores)
+  # Save PC1 scores to file
+  write.table(pc5_scores, file = paste0("pc5_scores_marine/pc5_scores_", tree_name, ".tsv"), sep = "\t", row.names = TRUE, col.names = TRUE)
+  
+}
